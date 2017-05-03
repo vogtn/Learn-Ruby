3a10524456026232d67ef2f1d99d3d3769d712e3
vogtn <nicolasjvogt@gmail.com>
2017-05-02 19:42:56 -0700

    up to exercise 5

---
+++ ex4.rb
@@ -0,0 +1,8 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+
+puts "There are #{cars} cars available."
+puts "there are #{space_in_a_car} spaces in a car"
+puts "There are #{cars - drivers} cars available"
+
---
+++ ex5.rb
@@ -0,0 +1,9 @@
+my_name = 'grapes'
+my_age = 'hello'
+my_eyes = 'hazel'
+my_height = 73 #inches
+my_weight = 180
+
+puts "Lets talk about #{my_name}"
+
+puts "if I add my height #{my_height} and my weight #{my_weight} I get #{my_height + my_weight}"
